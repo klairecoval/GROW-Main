de5183345f75b78aca1b3c6b674fd6700e396315
jtm6735 <jtm6735@rit.edu>
2020-02-01 17:21:19 -0500

    Added starting code to fill out the prompts for the thoughtDrop page

---
+++ client/app/thoughtDrop.js
@@ -0,0 +1,19 @@
+const prompts = document.querySelector('#promptSlot');
+
+const PromptSetup = () =>{
+    return(
+        <div classname="promptList">
+            <h1>Prompt:1</h1>
+            <h1>Prompt:2</h1>
+            <h1>Prompt:3</h1>
+            <h1>Prompt:4</h1>
+        </div>
+     );
+};
+
+const setup = () => {
+    ReactDOM.render(
+        <PromptSetup />,
+        document.querySelector("promptSlot");
+    )
+}
